a2e430ade6ab4897fd1dc685b0e7501f41eb7889
cgoetting30 <cgoetting30@gmail.com>
2016-02-28 23:08:00 -0500

    tapper game added

--- tapper game/ViewController.swift
+++ tapper game/ViewController.swift
@@ -9,7 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    var i = Int(0)
+    
+    @IBOutlet var label: UILabel!
+    
+    @IBOutlet var taps: UITextField!
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        let numberTaps: Int32! = Int32(taps.text!)
+       
+        label.text = "\(numberTaps) taps left!"
+        
+    }
+    
+    @IBAction func buttonTap(sender: AnyObject) {
+        
+        let numberTaps = Int32(taps.text!)
+        
+        if numberTaps != nil {
+        
+        let tapsLeft = numberTaps! - i
+        
+        if tapsLeft > 0 {
+            
+            label.text = "\(tapsLeft) taps left!"
+        
+        
+            i++ }
+        else {
+            
+            taps.text = nil
+            label.text = nil
+            i = 0
+            self.view.userInteractionEnabled = false
+        }
+        }
+        else {
+            
+            label.text = "Please enter a number"
+        }
+        
+        self.view.userInteractionEnabled = true
+    
+    }
+    
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
